0eb3f04cf8223fa5f087a5860e6258ebdcc26e0c
unknown <minldft@gmail.com>
2020-06-11 20:47:00 +0300

    adding redux-thunk

--- src/pages/homepage/home-page.pages.jsx
+++ src/pages/homepage/home-page.pages.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { createStructuredSelector } from "reselect";
 
 import ShopItem from "../../components/shop-item/shop-item.components";
+import { fetchCollStartAsync } from "../../redux/product/product.actions";
+import {
+  selectColl,
+  selectIsFetching,
+  selectCollect,
+} from "../../redux/product/produkt.selectors";
+import { connect } from "react-redux";
 
-const HomePage = () => <ShopItem />;
+class HomePage extends React.Component {
+  componentDidMount() {
+    const { fetchCollStartAsync } = this.props;
+    fetchCollStartAsync();
+  }
 
-export default HomePage;
+  render() {
+    const { collect, loading } = this.props;
+    // loading ? console.log("loading") : console.log(collect);
+
+    if (loading) {
+      console.log("loading ");
+    } else {
+      const a = typeof collect;
+      console.log("a=", a, "b=", collect.coll);
+    }
+
+    return <ShopItem />;
+  }
+}
+
+const mapStateToProps = createStructuredSelector({
+  collect: selectColl,
+  loading: selectIsFetching,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchCollStartAsync: () => dispatch(fetchCollStartAsync()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
---
+++ src/redux/product/product.actions.js
@@ -0,0 +1,26 @@
+import ProduktActionTypes from "./produkt.types";
+
+export const fetchCollStart = () => ({
+  type: ProduktActionTypes.FETCH_COLL_START,
+});
+
+export const fetchCollSuccess = (collect) => ({
+  type: ProduktActionTypes.FETCH_COLL_SUCCESS,
+  payload: collect,
+});
+
+export const fetchCollFailure = (errorMessage) => ({
+  type: ProduktActionTypes.FETCH_COLL_FAILURE,
+  payload: errorMessage,
+});
+
+export const fetchCollStartAsync = () => {
+  return (dispatch) => {
+    dispatch(fetchCollStart());
+
+    fetch("http://localhost:3000/shop")
+      .then((res) => res.json())
+      .then((data) => dispatch(fetchCollSuccess(data)))
+      .catch((errorMessage) => dispatch(fetchCollFailure(errorMessage)));
+  };
+};
