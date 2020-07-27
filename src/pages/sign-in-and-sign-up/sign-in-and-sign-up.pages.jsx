d84ad475a698b9fdf02dde9d11fdc714241a7531
unknown <minldft@gmail.com>
2020-07-27 21:30:12 +0300

    adding a part of signin and register form

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.pages.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.pages.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 
-const SignInAndSignUp = () => <h1>SIGN IN</h1>;
+import FormInput from "../../components/form-input/form-input.component";
+
+const SignInAndSignUp = () => (
+  <div className="sign-in-and-sign-up">
+    <FormInput
+      name="email"
+      type="email"
+      value="email"
+      //   handleChange={handleChange}
+      label="email"
+      required
+    />
+    <FormInput
+      name="password"
+      type="password"
+      value="password"
+      //   handleChange={handleChange}
+      label="password"
+      required
+    />
+  </div>
+);
 
 export default SignInAndSignUp;
