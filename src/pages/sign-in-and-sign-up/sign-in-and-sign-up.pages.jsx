e2c6521a7f7d7fa24b6355d79fede7474bf8056c
unknown <minldft@gmail.com>
2020-07-28 11:32:15 +0300

    adding style to sign-in-and-sign-up

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.pages.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.pages.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 import FormInput from "../../components/form-input/form-input.component";
+import "./sign-in-and-sign-up.styles.scss";
 
 const SignInAndSignUp = () => (
   <div className="sign-in-and-sign-up">
+    <h3>SIGN IN</h3>
     <FormInput
       name="email"
       type="email"
