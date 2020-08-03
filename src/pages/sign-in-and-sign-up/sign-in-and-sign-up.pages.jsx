d6d5e442b336d8144ebe98b959b45a03a8a08dff
unknown <minldft@gmail.com>
2020-08-03 09:19:43 +0300

    fix sign-in-and-sign-up-component

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.pages.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.pages.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { setCollection } from "../../redux/shop/shop.actions";
@@ -6,37 +6,59 @@ import { setCollection } from "../../redux/shop/shop.actions";
 import FormInput from "../../components/form-input/form-input.component";
 import "./sign-in-and-sign-up.styles.scss";
 
-const SignInAndSignUp = ({ setCollection }) => (
-  <div className="sign-in-and-sign-up">
-    <h3>SIGN IN</h3>
-    <FormInput
-      name="email"
-      type="email"
-      value=""
-      //   handleChange={handleChange}
-      label="email"
-      required
-    />
-    <FormInput
-      name="password"
-      type="password"
-      value=""
-      //   handleChange={handleChange}
-      label="password"
-      required
-    />
-    <button type="submit">войти</button>
-    <br />
-    <Link
-      to="/register"
-      className="register"
-      onClick={() => setCollection({ title: "Регистрация" })}
-    >
-      регистрация
-    </Link>
-    {/* <h6 className="reg">регистрация</h6> */}
-  </div>
-);
+const SignInAndSignUp = ({ signInStart, setCollection }) => {
+  const [userCredentials, setUserCredentials] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = userCredentials;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    signInStart(email, password);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setUserCredentials({ ...userCredentials, [name]: value });
+  };
+
+  return (
+    <div className="sign-in-and-sign-up">
+      <h3>SIGN IN</h3>
+      <form onSubmit={handleSubmit}></form>
+      <FormInput
+        name="email"
+        type="email"
+        value={email}
+        handleChange={handleChange}
+        label="email"
+        required
+      />
+      <FormInput
+        name="password"
+        type="password"
+        value={password}
+        handleChange={handleChange}
+        label="password"
+        required
+      />
+      <button type="submit">войти</button>
+      <form />
+      <br />
+      <Link
+        to="/register"
+        className="register"
+        onClick={() => setCollection({ title: "Регистрация" })}
+      >
+        регистрация
+      </Link>
+    </div>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => ({
   setCollection: (item) => dispatch(setCollection(item)),
